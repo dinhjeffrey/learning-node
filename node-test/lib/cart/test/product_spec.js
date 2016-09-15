ef98e0d1a1a2f938a31b22e2eff9aa5272820415
jeffrey dinh <dinhjeffreyy@gmail.com>
2016-09-15 08:00:04 -0700

    first commit - with mocha test, js libraries, plugins

---
+++ node-test/lib/cart/test/product_spec.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Jeffrey on 9/15/16.
+ */
+
+var assert = require("assert");
+
+describe("A Feature", function() {
+    describe("A scenario", function() {
+        it("does this thing", function() {
+            assert.ok(2===2);
+        });
+    });
+});
